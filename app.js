50f43fbf75f997bb7ffa7bea499ee8db6d97f3b1
Mallikharjuna Rao Katta <kattamallikharjuna1226@gmail.com>
2024-07-21 21:49:23 +0530

    handling-different-routes

--- app.js
+++ app.js
@@ -2,5 +2,16 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
+app.use('/', (req, res, next)=> {
+    console.log("This always run!");
+    next();
+});
+app.use('/add-product', (req, res, next)=> {
+    res.send('<form action="/product" method="post"><input type="text" name="title"/><button type="submit">Add product</button></form>');
+});
+app.use('/product', (req, res, next)=> {
+    console.log(req.body);
+    res.redirect('/');
+});
 const server = http.createServer(app);
 server.listen("3000");
