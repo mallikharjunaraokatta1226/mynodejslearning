90cd844a9ecc796dc46f5ce0a8bd87803a855d2c
Mallikharjuna Rao Katta <kattamallikharjuna1226@gmail.com>
2024-07-21 21:59:09 +0530

    parsing-incoming-requests

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use('/', (req, res, next)=> {
-    console.log("This always run!");
-    next();
-});
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/add-product', (req, res, next)=> {
     res.send('<form action="/product" method="post"><input type="text" name="title"/><button type="submit">Add product</button></form>');
 });
 app.use('/product', (req, res, next)=> {
     console.log(req.body);
     res.redirect('/');
+});
+app.use('/', (req, res, next)=> {
+    res.send('Hello from express js!');
 });
 const server = http.createServer(app);
 server.listen("3000");
