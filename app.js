d07f3bb5b3debe98235f37b3c60cc4976965771f
Mallikharjuna Rao Katta <kattamallikharjuna1226@gmail.com>
2024-07-22 09:41:52 +0530

    using-express-router

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-const http = require('http');
 const express = require('express');
-const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
+const bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({extended: false}));
-app.use('/add-product', (req, res, next)=> {
-    res.send('<form action="/product" method="post"><input type="text" name="title"/><button type="submit">Add product</button></form>');
-});
-app.use('/product', (req, res, next)=> {
-    console.log(req.body);
-    res.redirect('/');
-});
-app.use('/', (req, res, next)=> {
-    res.send('Hello from express js!');
-});
-const server = http.createServer(app);
-server.listen("3000");
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.listen("3000");
